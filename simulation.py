90741744ab1d29f51e161fba3251b926d63f651e
Latrokles <latrokles@samuraihippo.com>
2008-11-08 00:36:18 -0500

    Changed simulation to 2D only DONE 	-Changed everything to 2D for now 	-particles get velocity with mouse drag
    TODO
    	-try to use the new pyglet event loop... cant get it to work
    	-finish all the other stuff

--- simulation.py
+++ simulation.py
@@ -30,18 +30,43 @@ class Simulation:
 		self.gravity = gravity
 		self.drag    = drag
 		self.particles  = []
-		self.window.on_mouse_press = self.on_mouse_press
+		self.temp_dx = 0
+		self.temp_dy = 0
+		self.window.on_mouse_release = self.on_mouse_release
+		self.window.on_mouse_drag    = self.on_mouse_drag
+		self.window.on_draw          = self.on_draw
 
 	def __repr__(self):
-		return 'Simulation parameters:\nGravity:\t%d\nDrag:\t%d' % (self.gravity, self.drag)
+		return 'Simulation parameters:\nGravity:\t%d\nDrag:\t\t%d' % (self.gravity, self.drag)
 
+	#Event Handlers for pyglet window
 	def on_mouse_press(self, x, y, button, modifiers):
-		self.particles.append(Rectangle(x, y, 0, Vector([0,0,0]), 10, 10))
+		raise NotImplementedError
+	
+	def on_mouse_release(self, x, y, button, modifiers):
+		self.particles.append(Rectangle(x, y, self.temp_dx, self.temp_dy, 10, 10))
+		self.clear_velocity_data()
+
+	def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
+		self.temp_dx = dx
+		self.temp_dy = dy
 	
 	def on_mouse_motion(self, x, y, dx, dy):
-		raise NotImplementedError
+		raise NotImpplementedError
+		#print '(%d, %d):' % (self.temp_dx, self.temp_dy)
+	#End of Event Handlers
+
+	def clear_velocity_data(self):
+		self.temp_dx = 0
+		self.temp_dy = 0
+
+	def update_particles(self):
+		"""Updates the position of every particle in the simulation"""
+		for particle in self.particles:
+			particle.update_coordinates()
 
 	def draw_particles(self):
+		"""Draws all particles in the simulation"""
 		for particle in self.particles:
 			particle.draw()
 
@@ -50,33 +75,36 @@ class Simulation:
 			self.window.dispatch_events()
 			glClear(GL_COLOR_BUFFER_BIT)
 			glLoadIdentity()
-
+			self.update_particles()
 			self.draw_particles()
-
 			self.window.flip()
+
+	def on_draw(self):
+		self.window.clear()
+		self.update_particles()
+		self.draw_particles()
 
 class Particle:
 	"""Defines a single particle, either 2D or 3D"""
-	def __init__(self, x=0, y=0, z=0, direction=Vector([0,0,0])):
+	def __init__(self, x=0, y=0, dx=0, dy=0):
 		self.x = x
 		self.y = y
-		self.z = z
-		self.direction = direction
+		self.dx = dx
+		self.dy = dy
 
 	def get_coordinates(self):
-		return (self.x, self.y, self.z)
+		return (self.x, self.y)
 
-	def update_coordinates(self, dx=0, dy=0, dz=0):
-		self.x += dx
-		self.y += dy
-		self.z += dy
+	def update_coordinates(self):
+		self.x += self.dx
+		self.y += self.dy
 
 	def draw(self):
 		raise NotImplementedError
 
 class Rectangle(Particle):
-	def __init__(self, x=0, y=0, z=0, direction=Vector([0,0,0]), w=0, h=0):
-		Particle.__init__(self, x, y, z, direction)
+	def __init__(self, x=0, y=0, dx=0, dy=0, w=0, h=0):
+		Particle.__init__(self, x, y, dx, dy)
 		self.width  = w
 		self.height = h
 	
@@ -94,3 +122,4 @@ if __name__=='__main__':
 	simulation_test = Simulation(9.8, 2)
 	print simulation_test
 	simulation_test.run()
+	#pyglet.app.run()
